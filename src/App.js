d8bf4df06e4e214b2df6dd3677b862a68d806615
choongman <maxx@encar.com>
2019-08-20 14:33:56 +0900

    first commit todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TodoInput from './components/todo/TodoInput';
+import TodoList from './components/todo/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (value) => {
+    setTodos([...todos, { id: Date.now(), value: value, done: false, }]);
+  };
+
+  const handleChangeDone = (id) => {
+    const index = todos.findIndex(t => t.id === id);
+    setTodos(prev => [...prev.slice(0, index), { ...prev[index], done: !prev[index].done }, ...prev.slice(index + 1)]);
+  };
+
+  const handleDelete = (id) => {
+    setTodos(prev => prev.filter(t => t.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoInput onSubmit={handleSubmit} />
+      <TodoList todos={todos} onChangeDone={handleChangeDone} onDelete={handleDelete} />
     </div>
   );
 }
